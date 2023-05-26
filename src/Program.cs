205db64425a06ec4e45a797f6d3012c28961aafe
Engin Polat <polatengin@hotmail.com>
2023-05-25 19:15:50 -0700

    adding patients entrypoint

--- src/Program.cs
+++ src/Program.cs
@@ -2,5 +2,6 @@ var app = Helpers.Start(args);
 
 app.MapPost("/", RootHandler.Process).WithOpenApi();
 app.MapPost("/doctors", DoctorsHandler.Process).WithOpenApi();
+app.MapPost("/patients", PatientsHandler.Process).WithOpenApi();
 
 app.Run();
